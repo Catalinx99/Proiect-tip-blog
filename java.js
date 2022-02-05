0826da66f77c4d7ef52f3eb6393ec69be067b8aa
Catalinx99 <p.catalinv@yahoo.com>
2022-02-05 15:06:09 +0200

    Blog

---
+++ java.js
@@ -0,0 +1,24 @@
+function bigImg(x) {
+    x.style.height = "35px";
+    x.style.width = "35px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "30px";
+    x.style.width = "30px";
+  }
+
+
+
+  function bigImg2(x) {
+    x.style.height = "240px";
+    x.style.width = "355px";
+  }
+  
+  function normalImg2(x) {
+    x.style.height = "226px";
+    x.style.width = "369px";
+  }
+
+
+
